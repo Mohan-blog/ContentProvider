34e99c4ae8ecdb1390a7afea19426db1199f882b
Mohan-blog <mohan.seelamneni@gmail.com>
2020-01-20 15:16:34 +0530

    secondchange

--- app/src/main/java/com/adteccorp/demo/MainActivity.java
+++ app/src/main/java/com/adteccorp/demo/MainActivity.java
@@ -3,8 +3,12 @@ package com.adteccorp.demo;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    TextView tv;
+    Button btn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
